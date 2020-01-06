745622db6e7b56885a6105573bc848877946412f
yurisi0212 <Dev.yurisi@gmail.com>
2020-01-07 00:17:58 +0900

    コードを大幅変更

--- src/yurisi/main.php
+++ src/yurisi/main.php
@@ -11,48 +11,62 @@ use pocketmine\command\CommandSender;
 
 use pocketmine\event\Listener;
 
-use pocketmine\scheduler\PluginTask;
-use pocketmine\scheduler\CallbackTask;
-use pocketmine\scheduler\Task;
+use pocketmine\event\player\PlayerPreLoginEvent;
 
-use pocketmine\event\player\PlayerLoginEvent;
-
-use onebone\economyapi\EconomyAPI;
+use yurisi\Task\Sendtask;
 
 class main extends PluginBase implements Listener {
 
+   private $plugin= "PISB";
+
    public function onEnable() {
 	$this->getServer()->getPluginManager()->registerEvents($this, $this);
-	$this->plugin = "PISB";
-	$this->getLogger()->info("§b".$this->plugin."を開きました");
 	$this->getScheduler()->scheduleRepeatingTask(new Sendtask($this), 5);
-	$this->joho=array();
+	$this->getLogger()->info("§b".$this->plugin."を開きました");
    }
-   public function Login(PlayerLoginEvent $event){
-	$name=$event->getPlayer()->getName();
-	$this->joho[$name]=0;
+
+   public function Login(PlayerPreLoginEvent $event){
+	$tag=$event->getPlayer()->namedtag;
+	if ($tag->offsetExists($this->plugin)) {
+		$tag->setInt($this->plugin, 0);
+	}
 
    }
-   public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool{
-	$name = $sender->getPlayer()->getName();
-	switch($command->getName()){
-		case "joho":
-			if($sender instanceof Player){
-				if(!$this->joho[$name]==0){
-					$this->joho[$name]=0;
-					$sender->sendMessage("[PISB]§aONにいたしました。");
-					return false;
-				}else{
-					$this->joho[$name]=1;
-					$sender->sendMessage("[PISB]§aOFFにしました。");
-					return false;
-				}
-			}
-		break;
-	}
+   public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+
+	   switch ($command->getName()) {
+		   case "joho":
+			   if ($sender instanceof Player) {
+				   $tag = $sender->namedtag;
+				   if ($this->isOn($sender)===false) {
+					   $tag->setInt($this->plugin, 0);
+					   $sender->sendMessage("[".$this->plugin."]§aONにいたしました。");
+					   return true;
+				   } else {
+					   $tag->setInt($this->plugin, 1);
+					   $sender->sendMessage("[".$this->plugin."]§aOFFにしました。");
+					   return true;
+				   }
+			   }
+			   break;
+	   }
+   }
+
+   public function isOn(Player$player){
+	   $tag = $player->namedtag;
+	   if ($tag->offsetExists($this->plugin)) {
+		   if ($tag->getInt($this->plugin) == 0) {
+			   return true;
+		   } else {
+			   return false;
+		   }
+	   }else{
+	   	return true;
+	   }
    }
 
    public function onDisable() {
 		$this->getLogger()->info("§a".$this->plugin."を閉じました");
    }
+
 }
